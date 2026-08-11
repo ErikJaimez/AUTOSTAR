838593f328fe4a9b7c23ade227f4ee5c9b4d6b82
ErikJaimez <reik1923@gmail.com>
2026-08-11 16:37:37 -0600

    AUTI - AJUSTE MENÚ

--- src/components/landing/TopbarWidget.vue
+++ src/components/landing/TopbarWidget.vue
@@ -49,63 +49,32 @@ function smoothScroll(id) {
     >
         <i class="pi pi-bars !text-2xl"></i>
     </Button>
-        <div class="items-center bg-surface-0 dark:bg-surface-900 grow justify-between hidden lg:flex w-full px-12 lg:px-0 z-20 rounded-border">
-        <ul class="list-none p-0 m-0 flex lg:items-center select-none flex-col lg:flex-row cursor-pointer gap-8">
+        <div class="items-center bg-surface-0 dark:bg-surface-900 grow justify-between hidden lg:flex absolute lg:static top-full left-0 right-0 w-full lg:w-auto px-6 lg:px-0 py-4 lg:py-0 z-50 rounded-border shadow-lg lg:shadow-none border-b lg:border-b-0 border-surface">
+        <ul class="list-none p-0 m-0 flex lg:items-center select-none flex-col lg:flex-row cursor-pointer gap-2 lg:gap-8">
             <li>
-                <a @click="smoothScroll('inicio')" class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
+                <a @click="smoothScroll('inicio')" class="block px-4 py-3 lg:px-0 lg:py-4 text-surface-900 dark:text-surface-0 font-medium text-xl rounded-lg hover:bg-surface-100 dark:hover:bg-surface-800 lg:hover:bg-transparent transition-colors">
                     <span>Inicio</span>
                 </a>
             </li>
             <li>
-                <a @click="smoothScroll('cursos')" class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
+                <a @click="smoothScroll('cursos')" class="block px-4 py-3 lg:px-0 lg:py-4 text-surface-900 dark:text-surface-0 font-medium text-xl rounded-lg hover:bg-surface-100 dark:hover:bg-surface-800 lg:hover:bg-transparent transition-colors">
                     <span>Cursos</span>
                 </a>
             </li>
             <li>
-                <a @click="smoothScroll('opiniones')" class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
+                <a @click="smoothScroll('opiniones')" class="block px-4 py-3 lg:px-0 lg:py-4 text-surface-900 dark:text-surface-0 font-medium text-xl rounded-lg hover:bg-surface-100 dark:hover:bg-surface-800 lg:hover:bg-transparent transition-colors">
                     <span>Opiniones</span>
                 </a>
             </li>
             <li>
-                <a @click="smoothScroll('nosotros')" class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
+                <a @click="smoothScroll('nosotros')" class="block px-4 py-3 lg:px-0 lg:py-4 text-surface-900 dark:text-surface-0 font-medium text-xl rounded-lg hover:bg-surface-100 dark:hover:bg-surface-800 lg:hover:bg-transparent transition-colors">
                     <span>Nosotros</span>
                 </a>
             </li>
         </ul>
-        <div class="flex border-t lg:border-t-0 border-surface py-4 lg:py-0 mt-4 lg:mt-0 gap-2">
-            <Button
-  label="Facebook"
-  text
-  rounded
-  @click="goToFacebook"
-/>
-
+        <div class="flex border-t lg:border-t-0 border-surface py-4 lg:py-0 mt-4 lg:mt-0 gap-2 justify-center">
+            <Button label="Facebook" text rounded @click="goToFacebook" />
             <Button label="WhatsApp" @click="enviarWhatsAppGeneral" />
-                <!-- <Dialog header="Déjalo en nuestras manos o llámanos" v-model:visible="display" :breakpoints="{ '960px': '75vw' }" :style="{ width: '40vw' }" :modal="true">                 
-                    <div class="flex flex-col md:flex-row">
-                        <div class="w-full md:w-5/12 flex flex-col items-center justify-center gap-3 py-5">
-                            <div class="flex flex-col gap-2">
-                                <label for="name">Nombre</label>
-                                <InputText id="name" type="text" />
-                            </div>
-                            <div class="flex flex-col gap-2">
-                                <label for="number">WhatsApp</label>
-                                <InputText id="number" type="text" />
-                            </div>
-                            <div class="flex">
-                                <Button label="¡Quiero informes!" icon="pi pi-user" class="w-full max-w-[17.35rem] mx-auto"></Button>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-2/12">
-                            <Divider layout="vertical" class="!hidden md:!flex"><b>O</b></Divider>
-                        </div>
-                        <div class="w-full md:w-5/12 flex flex-col items-center justify-center py-5">
-                            <label for="number" class="text-l font-bold mb-1">55-54-71-38-95</label>
-                            <img src="@/assets/images/qr1.png" alt="Logo Autostar" class="h-40 w-auto" />
-                        </div>
-
-                    </div>     
-                </Dialog> -->
         </div>
     </div>
 </template>
